854ea906e634c75ccb98c493a662e784b7ad2721
Mas'ud <68595786+dev-onimoe@users.noreply.github.com>
2023-11-09 08:24:42 +0100

    Update MovieListTests.swift

--- MovieListTests/MovieListTests.swift
+++ MovieListTests/MovieListTests.swift
@@ -9,28 +9,107 @@ import XCTest
 @testable import MovieList
 
 final class MovieListTests: XCTestCase {
+    
+    var viewModel: ViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        viewModel = ViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        viewModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testKeyApi() {
+        
+        let expectation = XCTestExpectation(description: "Test request to retrieve key")
+        
+        var firstCheck = true
+        
+        viewModel.keyResponse.bind(completion: {response in
+            
+            //Check to see if the observer property is initialized
+            
+            if !firstCheck {
+                if let response = response {
+                    expectation.fulfill()
+                    XCTAssertTrue(response.object is String)
+                }else {
+                    XCTFail("Response should not be null")
+                }
+                
+            }else {
+                firstCheck = false
+            }
+            
+        })
+        
+        viewModel.getKey()
+        
+        wait(for: [expectation], timeout: 20.0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMovieApi() {
+        
+        let expectation = XCTestExpectation(description: "Test request to retrieve key")
+        
+        var firstCheck = true
+        
+        viewModel.poModelResponse.bind(completion: {response in
+            
+            //Check to see if the observer property is initialized
+            
+            if !firstCheck {
+                if let response = response {
+                    expectation.fulfill()
+                    XCTAssertTrue(response.object is [Movie])
+                }else {
+                    XCTFail("Response should not be null")
+                }
+                
+            }else {
+                firstCheck = false
+            }
+            
+        })
+        
+        viewModel.getPopular()
+        
+        wait(for: [expectation], timeout: 20.0)
+    }
+    
+    func testCastApi() {
+        
+        let expectation = XCTestExpectation(description: "Test request to retrieve key")
+        
+        var firstCheck = true
+        
+        viewModel.castResponse.bind(completion: {response in
+            
+            //Check to see if the observer property is initialized
+            
+            if !firstCheck {
+                if let response = response {
+                    expectation.fulfill()
+                    XCTAssertTrue(response.object is [Cast])
+                }else {
+                    XCTFail("Response should not be null")
+                }
+                
+            }else {
+                firstCheck = false
+            }
+            
+        })
+        
+        //Using a practical cast id example gotten from the api or else it fails
+        viewModel.getCast(id: "820525")
+        
+        wait(for: [expectation], timeout: 20.0)
     }
 
 }
+
